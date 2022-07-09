9f1783bc6d0347782ab310fbd55d6ed48a1fa3d2
Muhammad Edo Wardaya <65476273+muhammadedowardaya@users.noreply.github.com>
2022-07-09 11:00:37 +0700

    update tampilan text saat di copy paste

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ pre.forEach((pre) => {
     pre.addEventListener("copy", (event) => {
         alert("anjay di copy euy :) ");
         const text = `
-        Aduh euy, Maaf ya gaes, 
-        kalian gak bisa copy paste code dari saya, 
-        ketik aja manual ehehehe...
+Aduh euy, Maaf ya gaes, 
+kalian gak bisa copy paste code dari saya, 
+ketik aja manual ehehehe...
         `;
         event.clipboardData.setData("text/plain", text);
         event.preventDefault();
