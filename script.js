44caf9631a5f02f411d3a5c65236a7c47cfd844d
Muhammad Edo Wardaya <65476273+muhammadedowardaya@users.noreply.github.com>
2022-07-09 10:44:25 +0700

    upload semua file ke repository

---
+++ script.js
@@ -0,0 +1,14 @@
+const pre = document.querySelectorAll("pre");
+
+pre.forEach((pre) => {
+    pre.addEventListener("copy", (event) => {
+        alert("anjay di copy euy :) ");
+        const text = `
+        Aduh euy, Maaf ya gaes, 
+        kalian gak bisa copy paste code dari saya, 
+        ketik aja manual ehehehe...
+        `;
+        event.clipboardData.setData("text/plain", text);
+        event.preventDefault();
+    });
+});
